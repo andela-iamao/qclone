7049c2a52e9e282af5d2de372677a67026f9e9df
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2017-10-19 05:16:31 +0100

    hotfix(message): save base url on index page visit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Auth from '../client/components/Auth';
 import Home from '../client/components/Home';
 import Bulma from '../client/components/Bulma';
 
+const SOCKET_URI = process.env.API_URI;
+
 class IndexPage extends React.Component {
 
   constructor(props) {
@@ -11,6 +13,18 @@ class IndexPage extends React.Component {
       isLoggedIn: null
     };
     this.checkAuthStatus = this.checkAuthStatus.bind(this);
+  }
+
+  componentWillMount() {
+    if (SOCKET_URI) {
+      this.setState({ socketURI: SOCKET_URI });
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.socketURI) {
+      window.localStorage.setItem('socketURI', this.state.socketURI);
+    }
   }
 
   checkAuthStatus(loggedIn) {
